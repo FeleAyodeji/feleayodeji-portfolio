a275b8eb6ba920f5b47e9171d8ab03c71e7ab382
FeleAyodeji <feledeji@gmail.com>
2025-04-29 23:48:12 +0100

    Integrate font and hover effects in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,37 +4,37 @@ const Navbar = () => {
   return (
     <nav className="flex justify-between items-center px-8 py-4 bg-[#2B2B3C] text-white">
       {/* Logo */}
-      <div className="font-bold text-xl">FA</div>
+      <div className="font-montseratt font-bold italic text-xl">FA</div>
 
       {/* Navigation Links */}
       <div className="flex space-x-8">
         <Link
           to="/"
-          className="hover:text-[#BDBDFF] border-b-2 border-transparent hover:border-[#BDBDFF]"
+          className=" font-poppins font-medium hover:text-[#BDBDFF] border-b-2 border-transparent hover:border-[#BDBDFF]"
         >
           Home
         </Link>
         <Link
           to="/services"
-          className="hover:text-[#BDBDFF] border-b-2 border-transparent hover:border-[#BDBDFF]"
+          className="font-poppins font-medium hover:text-[#BDBDFF] border-b-2 border-transparent hover:border-[#BDBDFF]"
         >
           Service
         </Link>
         <Link
           to="/portfolio"
-          className="hover:text-[#BDBDFF] border-b-2 border-transparent hover:border-blue-[#BDBDFF]"
+          className="font-poppins font-medium hover:text-[#BDBDFF] border-b-2 border-transparent hover:border-blue-[#BDBDFF]"
         >
           Portfolio
         </Link>
         <Link
           to="/blog"
-          className="hover:text-#BDBDFF[] border-b-2 border-transparent hover:border-[#BDBDFF]"
+          className="font-poppins font-medium hover:text-[#BDBDFF] border-b-2 border-transparent hover:border-[#BDBDFF]"
         >
           Blog
         </Link>
         <Link
           to="/contact"
-          className="hover:text-[#BDBDFF] border-b-2 border-transparent hover:border-[#BDBDFF]"
+          className="font-poppins font-medium hover:text-[#BDBDFF] border-b-2 border-transparent hover:border-[#BDBDFF]"
         >
           Contact
         </Link>
@@ -42,7 +42,7 @@ const Navbar = () => {
 
       {/* Resume Button */}
       <div>
-        <button className="px-4 py-2 border border-[] rounded hover:bg-[] hover:text-[] transition">
+        <button className="font-poppins font-normal px-4 py-2 border border-[] rounded hover:bg-[] hover:text-[] transition">
           Resume
         </button>
       </div>
